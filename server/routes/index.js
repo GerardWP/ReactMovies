4e6f6e534866bb144790e992725c27360806ec00
Gerard Pelly <gerardwp@gmail.com>
2019-11-08 23:15:55 +1100

    server connection working - not yet successfully storing info in mongo for password

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../database/models/user");
 
 router.post("/", (req, res) => {
-  if (req.body) {
-    console.log("i think it worked!");
-    console.log(req.body);
-    res.json(req.body);
-  } else console.log("it didnt work");
+  const { username, password } = req.body;
+  console.log("HERE IT IS");
+  console.log(username, password);
+  res.json(req.body); // work up until this point - res.json sends the right info, but nothing below here console.logs.
+  // ADD VALIDATION
+  User.findOne({ _username: username }, (err, user) => {
+    console.log("HERE IT IS 2");
+    if (err) {
+      console.log("User.js post error: ", err);
+    } else if (user) {
+      res.json({
+        error: `Sorry, already a user with the username: ${username}`
+      });
+    } else {
+      console.log("HERE IT IS 3");
+      const newUser = new User({
+        username: username,
+        password: password
+      });
+      console.log("here..where it at");
+      newUser.save((err, savedUser) => {
+        console.log("here too...here it at");
+        if (err) return res.json(err);
+        res.json(savedUser);
+      });
+    }
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/index");
+const session = require("express-session");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(routes);
 
 app.use(routes);
-// Code for heroku
-// Code for mongoose
-// code for passport
+
+// Sessions
+app.use(
+  session({
+    secret: "bonox-chicken",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
